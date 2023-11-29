22308cb8e0268f168ac4da14b9b96c4f319bd71b
future <futurekgaphola@gmail.com>
2023-11-29 12:46:04 +0000

    first commit

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['./assets/fonts','./assets/fonts/Bebas_Neue','./assets/fonts/Itim','./assets/fonts/Nunito_Sans','./assets/fonts/Open_Sans','./assets/fonts/Pacifico','./assets/fonts/Roboto'],
+  };
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import {Alert, Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,TouchableWithoutFeedback,Keyboard
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {
   GoogleSignin,
@@ -10,6 +18,8 @@ GoogleSignin.configure({
   webClientId:
     '534881027218-0k23t5grdmpdpeq9hl6nm7sdopuqnu1q.apps.googleusercontent.com',
 });
+import Icon from 'react-native-vector-icons/Zocial';
+import Octicons from 'react-native-vector-icons/Octicons';
 import {Button, Card, TextInput, Switch} from 'react-native-paper';
 
 async function onGoogleButtonPress() {
@@ -44,32 +54,39 @@ const Login = () => {
   const [isSwitchOn, setIsSwitchOn] = React.useState(false);
 
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const [secure,setsecure]=React.useState(true);
 
   return (
-    <ScrollView contentContainerStyle={{flexGrow:1,backgroundColor: '#fff'}}>
+    <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+      <ScrollView contentContainerStyle={{flexGrow: 1, backgroundColor: '#fff'}}>
       <Image
         style={{width: '100%', height: 300, objectFit: 'contain'}}
         source={require('../assets/banner.png')}
       />
-
-      <Text style={{fontSize: 25}}>Good To See</Text>
-      <Text style={{fontSize: 25}}>you Back</Text>
+      <Text style={styles.catchyText}>Good To See</Text>
+      <Text style={styles.catchyText}>you Back</Text>
       <Card style={{padding: 10, backgroundColor: '#FFF', margin: 10}}>
+        
         <TextInput
           mode="outlined"
           label="Enter email"
-          inputMode='email'
+          inputMode="email"
           placeholder="Enter email"
-          left={<TextInput.Icon icon="eye" />}
+          placeholderTextColor={'#C7C7C7'}
+          left={<TextInput.Icon icon={()=>(<Icon name="email" size={24} color="black" />)}/>}
         />
         <TextInput
           mode="outlined"
           label="Enter password"
           placeholder="Enter password"
-          secureTextEntry
-          right={<TextInput.Icon icon="eye" />}
+          placeholderTextColor={'#C7C7C7'}
+          secureTextEntry={secure}
+          right={<TextInput.Icon icon={()=>(secure? <Octicons onPress={()=>secure? setsecure(false) : setsecure(true)} name="eye-closed" size={24} color="black" /> : <Octicons onPress={()=>secure? setsecure(false):setsecure(true)} name="eye" size={24} color="black" />)} />}
         />
-        <Text style={{alignSelf: 'flex-end'}}>Forgot password</Text>
+        <TouchableOpacity>
+        <Text style={{alignSelf: 'flex-end',fontFamily:'Itim-Regular',fontSize:20,color:'black',marginTop:7}}>Forgot password</Text>
+        </TouchableOpacity>
+        
         <Button
           textColor="#fff"
           style={{borderRadius: 8, backgroundColor: '#FFBD11', marginTop: 5}}
@@ -85,7 +102,8 @@ const Login = () => {
             alignItems: 'center',
             backgroundColor: '#F6F5F2',
             borderRadius: 8,
-            zIndex: 6,
+            elevation: 6,
+            
           }}>
           <GoogleSigninButton
             size={GoogleSigninButton.Size.Icon}
@@ -97,24 +115,34 @@ const Login = () => {
           />
           <Text
             style={{alignSelf: 'center', color: '#FFBD11', fontWeight: '600'}}>
-            Sign with Google
+            Sign in with Google
           </Text>
         </TouchableOpacity>
-        <View style={{flexDirection: 'row'}}>
+        <View style={{flexDirection: 'row',marginTop:5}}>
           <Text>Remember me </Text>
           <Switch
             thumbColor={'#FFBD11'}
+            color='#FFBD11'
             value={isSwitchOn}
             onValueChange={onToggleSwitch}
           />
         </View>
       </Card>
-      <View style={{flexDirection: 'row'}}>
+      <View style={{flexDirection: 'row',marginTop:5,marginLeft:10}}>
         <Text>Don't have an account? </Text>
+        <TouchableOpacity>
         <Text style={{color: '#FFBD11'}}>Signup</Text>
+        </TouchableOpacity>
+        
       </View>
     </ScrollView>
+    </TouchableWithoutFeedback>
+    
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  catchyText: {fontSize: 28,fontFamily:'Itim-Regular',color:'black',textShadowColor:'#DCDCDC',textShadowRadius:4,marginLeft:10},
+});
